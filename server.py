67ac31fe9d35de294ae7706f5e75d45e83213fe0
nkrasnov2002-droid <n.krasnov2002@gmail.com>
2026-02-28 17:09:30 +0500

    Refactor server.py for improved readability

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -77,3 +78,4 @@ if __name__ == '__main__':
     import os
 port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
+
