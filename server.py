c49e0c1de823f22250cf847dca928d0bb136f48b
nkrasnov2002-droid <n.krasnov2002@gmail.com>
2026-02-28 17:05:49 +0500

    Modify server to use environment variable for port
    Updated server to run on environment-defined port.

--- server.py
+++ server.py
@@ -73,4 +73,7 @@ def clear_cart(user_id):
 # 🚀 ЗАПУСК СЕРВЕРА
 # =========================
 if __name__ == '__main__':
-    app.run(port=5000)
+
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
