4338eed41552342a30b31f54dc85bafd01cc5caf
nkrasnov2002-droid <n.krasnov2002@gmail.com>
2026-03-04 15:30:10 +0500

    Update server.py

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ def calculate_distance(lat1, lon1, lat2, lon2):
     d_lat = math.radians(lat2 - lat1)
     d_lon = math.radians(lon2 - lon1)
 
-    a = math.sin(d_lat/2)2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(d_lon/2)2
+    a = math.sin(d_lat/2)**2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(d_lon/2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
 
     return R * c
@@ -354,3 +354,4 @@ def webhook():
                 "reply_markup":keyboard
             }
         )
+
