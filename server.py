9ab083be7238bfed2413a0cd9c963af2292c8a64
nkrasnov2002-droid <n.krasnov2002@gmail.com>
2026-03-07 16:45:35 +0500

    Update server.py

--- server.py
+++ server.py
@@ -154,7 +154,7 @@ for name,item in cart.items():
     subtotal = item["price"] * item["qty"]
     total += subtotal
 
-    text += f"{i}️⃣ {name} x {item['qty']} — {subtotal} ₽\n"
+    text += f"{i}️. {name} x {item['qty']} — {subtotal} ₽\n"
 
     index_map[str(i)] = name
     i += 1
@@ -406,6 +406,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
 
     app.run(host="0.0.0.0",port=port)
+
 
 
 
